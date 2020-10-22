c4792572ba1d45192cae6928e24a3a6ef2ee4fdc
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-10-21 23:42:15 -0700

    Lec 03

--- Lec 03/Cirno's Perfect Math Class 琪露諾的完美算術教室.c
+++ Lec 03/Cirno's Perfect Math Class 琪露諾的完美算術教室.c
@@ -6,4 +6,4 @@ int main  ()
 	scanf("%d %d %d %d",&a,&b,&c,&d);
 	printf("%04d %04d %04d",a,a-b+c,a-b+c-d);
 	retirn 0;
-}
+} 
--- Lec 03/Scanf Format Problem.c
+++ Lec 03/Scanf Format Problem.c
@@ -6,4 +6,4 @@ int main ()
 	scanf("%d/%d**%d/)%d",&a,&b,&c,&d);
 	printf("%d",a+b+c+d);
 	return 0;
-}
+} 
