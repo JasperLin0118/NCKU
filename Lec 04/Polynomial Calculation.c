4e45ceb708c8ded18cd2e5a4a74536492c0c2e77
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-10-22 00:08:44 -0700

    Lec 04

--- Lec 04/Polynomial Calculation.c
+++ Lec 04/Polynomial Calculation.c
@@ -5,6 +5,6 @@ int main ()
 {
 	float x;
 	scanf("%f",&x);
-	printf("%f", 7*pow(x,4)-8*pow(x,3)-pow(x,2)+6*x-22);
+	printf("%.1f", 7*pow(x,4)-8*pow(x,3)-pow(x,2)+6*x-22);
 	return 0;
 }	
--- Lec 04/Wait for the Class Ending.c
+++ Lec 04/Wait for the Class Ending.c
@@ -5,6 +5,6 @@ int main ()
 	int hour,minute;
 	scanf("%d %d",&hour,&minute);
 	minute = (minute>=10) ? hour++,minute-10 : minute+50 ;
-	printf("%02d:%02d",hour,minute);
+	printf("%02d:%02d\n",hour,minute);
 	return 0;
 }
