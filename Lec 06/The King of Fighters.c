024583861cd47386fb29b546488e3aa0c7dd8a20
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-10-23 14:20:47 +0800

    Lec 06

--- Lec 06/The King of Fighters.c
+++ Lec 06/The King of Fighters.c
@@ -4,12 +4,12 @@ int main ()
 {
 	int ha,hb,attack,tmp=0;
 	scanf("%d %d", &ha, &hb);
-	while(ha>0 || hb >0)
+	while(ha>0 && hb >0)
 	{
 		scanf("%d", &attack);
 		if(tmp++ % 2 == 0) hb -= attack;
 		else ha -= attack;
 	}
-	printf("%c", ha>0 ? 'B' : 'A');
+	printf("%c", ha>0 ? 'A' : 'B');
 	return 0;
 }
