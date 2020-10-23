19c3e678b67b1d2e29fab372759f87888fa3bc22
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-10-23 14:46:48 +0800

    Lec 05

--- Lec 05/Find the Median.c
+++ Lec 05/Find the Median.c
@@ -3,7 +3,7 @@
 int main ()
 {
 	int a,b,c,med;
-	scanf("%d %d %d",&a,%b,%c);
+	scanf("%d %d %d",&a,&b,&c);
 	med = a;
 	if( (a<=b && b<=c) || (c<=b && b<=a) ) med = b;
 	if( (a<=c && c<=b) || (b<=c && c<=a) ) med = c;
