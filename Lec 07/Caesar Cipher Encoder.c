edbf3628c6b0552968acf2a36478016babf27220
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-10-29 20:33:33 +0800

    Lec 07

--- Lec 07/Caesar Cipher Encoder.c
+++ Lec 07/Caesar Cipher Encoder.c
@@ -5,10 +5,10 @@ int main ()
 	int move;
 	char ch;
 
-	scanf("%d %c", &move, &ch);
+	scanf("%d%c", &move, &ch);
 	move = (move<0) ? move%26+26 : move%26;
 
-	while( scanf("%c", &ch) != EOF ) 
+	while( scanf("%c", &ch) != EOF && ch!='\n') 
 	{
 		if(ch>='A' && ch<='Z') printf("%c",'A'+(ch-'A'+move)%26);
 		else if(ch>='a' && ch<='z') printf("%c",'a'+(ch-'a'+move)%26);
