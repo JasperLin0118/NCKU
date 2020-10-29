8e8e7082a731615f0239f0371cb0bf73b07e247d
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-10-29 23:14:36 +0800

    Lec 07

--- Lec 07/Trim Redundant Spaces.c
+++ Lec 07/Trim Redundant Spaces.c
@@ -2,8 +2,8 @@
 
 int main ()
 {
-	char words[180];
-	scanf("%[^\n]",words);
+	char words[1000];
+	scanf("%1000[^\n]",words);
 	for(int i=0;words[i] != '\0';i++)
 	{
 		if(words[i]==' ' && words[i+1]!=' ') printf(" ");
