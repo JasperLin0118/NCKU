4dd9170d4b4bbe78ddb8f6b45350c8b27aa98ba3
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-11-21 22:46:03 +0800

    midtest1

---
+++ Lec 09/Balu Sequence.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+long B(long a)
+{
+	if(a == 1) return 2;
+	return (2*B(a-1)+3*B(a-2))%1000000007;
+}
+
+int main()
+{
+	int i;
+	scanf("%d", &i);
+	printf("%ld", B(i));
+
+	return 0;
+}
