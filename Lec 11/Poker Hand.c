d88dc31db46a45310fedaaa82c9055cce0d42d91
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-12-06 16:55:50 +0800

    Lec 11

--- Lec 11/Poker Hand.c
+++ Lec 11/Poker Hand.c
@@ -4,9 +4,9 @@
 
 void sort(int ranks[people+1][cards+1],int suits[people+1][cards+1], int person)
 {
-	for(int i=1;i<=cards;i++)
+	for(int i=1;i<cards;i++)
     	{
-       	        for(int j=i+1;j<=cards+1;j++)
+       	        for(int j=i+1;j<=cards;j++)
        	        {
        	              if(ranks[person][j]<ranks[person][i]) 
 		      {
@@ -14,71 +14,56 @@ void sort(int ranks[people+1][cards+1],int suits[people+1][cards+1], int person)
 			      suits[person][j] ^= suits[person][i] ^= suits[person][j] ^= suits[person][i];
 		      }
        	        }
+		for(int j=i+1;j<=cards;j++)
+		{
+			if(ranks[person][i]==ranks[person][j] && suits[person][j]<suits[person][i]) 
+                              suits[person][j] ^= suits[person][i] ^= suits[person][j] ^= suits[person][i];
+		}
    	}
 }
 
-int straight(int ranks[people+1][cards+1], int person)
-{
-	sort(ranks, suits, person);
-	
-	for(int i=1;i<cards;i++)
-	{
-		if(ranks[person][i]+1 != ranks[person][i+1]) return 0;
-	}
-
+int straight(int ranks[people+1][cards+1],int suits[people+1][cards+1], int person)
+{	
+	for(int i=1;i<cards;i++) if(ranks[person][i]+1 != ranks[person][i+1]) return 0;	
 	return 1;
 }
 
-int flush(int suits[people+1][cards], int person)	//same flower
+int flush(int suits[people+1][cards+1], int person)	
 {
-	for(int i=1;i<cards;i++)
-	{
-		if(suits[person][i] != suits[person][i+1]) return 0;
-	}
+	for(int i=1;i<cards;i++) if(suits[person][i] != suits[person][i+1]) return 0;
 	return 1;
 }
 
-int four_of_a_kind(int ranks[people+1][cards+1], int person)
+int four_of_a_kind(int ranks[people+1][cards+1],int suits[people+1][cards+1], int person)
 {
-	sort(ranks, suits, person);
-
 	if(ranks[person][2]==ranks[person][3] && ranks[person][3]==ranks[person][4])
-	{
-		if(ranks[person][1]==ranks[person][2] && ranks[person][5]!=ranks[person][2]) return 1;
-                if(ranks[person][1]!=ranks[person][2] && ranks[person][5]==ranks[person][2]) return 1;
-	}
+		if(ranks[person][1]==ranks[person][2] || ranks[person][5]==ranks[person][2]) return 1;
 	return 0;
 }
 
-int full_house(int ranks[people+1][cards+1], int person)
+int full_house(int ranks[people+1][cards+1],int suits[people+1][cards+1], int person)
 {
-	sort(ranks,suits,person);
-
-	if(ranks[person][1]==ranks[person][2]&&ranks[person][2]==ranks[person][3] && ranks[person][4]==rands[person][5])
+	if(ranks[person][1]==ranks[person][2] && ranks[person][2]==ranks[person][3] && ranks[person][4]==ranks[person][5])
 		return 1;
-        if(ranks[person][3]==ranks[person][4]&&ranks[person][4]==ranks[person][5] && ranks[person][1]==rands[person][2])
+        if(ranks[person][3]==ranks[person][4] && ranks[person][4]==ranks[person][5] && ranks[person][1]==ranks[person][2])
 		return 1;
 	return 0;
 }
 
 int three_of_a_kind(int ranks[people+1][cards+1], int suits[people+1][cards+1], int person)
 {
-	sort(ranks,suits,person);
-
-        if(ranks[person][1]==ranks[person][2]&&ranks[person][2]==ranks[person][3] && ranks[person][4]!=rands[person][5])
+        if(ranks[person][1]==ranks[person][2] && ranks[person][2]==ranks[person][3])
 		return 1;
-        if(ranks[person][2]==ranks[person][3]&&ranks[person][3]==ranks[person][4] && ranks[person][1]!=rands[person][5])
+        if(ranks[person][2]==ranks[person][3] && ranks[person][3]==ranks[person][4])
 		return 1;
-        if(ranks[person][3]==ranks[person][4]&&ranks[person][4]==ranks[person][5] && ranks[person][1]==rands[person][2])
+        if(ranks[person][3]==ranks[person][4] && ranks[person][4]==ranks[person][5])
 		return 1;
 
 	return 0;
 }
 
-int two_pair(int ranks[people+1][cards+1], int person)
+int two_pair(int ranks[people+1][cards+1],int suits[people+1][cards+1], int person)
 {
-	sort(ranks,suits,person);
-
 	if(ranks[person][1]==ranks[person][2] && ranks[person][3]==ranks[person][4]) return 1;
         if(ranks[person][2]==ranks[person][3] && ranks[person][4]==ranks[person][5]) return 2;
         if(ranks[person][1]==ranks[person][2] && ranks[person][4]==ranks[person][5]) return 3;
@@ -86,14 +71,10 @@ int two_pair(int ranks[people+1][cards+1], int person)
 	return 0;
 }
 
-int pair(int ranks[people+1][cards+1], person)
+int pair(int ranks[people+1][cards+1],int suits[people+1][cards+1], int person)
 {
-	sort(ranks,suits,person);
-
-	if(ranks[person][1] == ranks[person][2]) return 1;
-        if(ranks[person][2] == ranks[person][3]) return 2;
-        if(ranks[person][3] == ranks[person][4]) return 3;
-        if(ranks[person][4] == ranks[person][5]) return 4;
+	for(int i=1;i<people;i++)
+		if(ranks[person][i] == ranks[person][i+1]) return i;
 	
 	return 0;	
 }
@@ -102,7 +83,7 @@ int pair(int ranks[people+1][cards+1], person)
 int main()
 {
 	int ranks[people+1][cards+1], suits[people+1][cards+1];
-	int ans[people+1][4]; 
+	int ans[people+1]; 
 
 	for(int i=1;i<=people;i++)
 	{
@@ -118,7 +99,7 @@ int main()
 				case 't': ranks[i][j] = 10;
 				 break;
 				case 'j': ranks[i][j] = 11;
-			 	 break;
+				 break;
 				case 'q': ranks[i][j] = 12;
 				 break;
 				case 'k': ranks[i][j] = 13;
@@ -138,124 +119,75 @@ int main()
                                  break;
                                 case 's': suits[i][j] = 4;
                                  break;
-			}
+			}	
+		}
+		sort(ranks,suits,i);
+	}
 
+
+	for(int i=1;i<=people;i++)
+	{
+		if(straight(ranks,suits,i)==1 && flush(suits,i)==1) 			//straight flush
+			ans[i] = 9000+10*ranks[i][5]+suits[i][5];
+
+		else if(four_of_a_kind(ranks,suits,i) == 1)				//four-of-a-kind
+			ans[i] = 8000+10*ranks[i][3];
+
+		else if(full_house(ranks,suits,i) == 1)					//full-house
+			ans[i] = 7000+10*ranks[i][3]+suits[i][3];
+
+		else if(flush(suits,i) == 1) 						//flush
+			ans[i] = 6000+10*ranks[i][5]+suits[i][5];
+		
+
+		else if(straight(ranks,suits,i) == 1)					//straight
+			ans[i] = 5000+10*ranks[i][5]+suits[i][5];
+		
+		else if(three_of_a_kind(ranks,suits,i) == 1)				//three-of-a-kind
+			ans[i] = 4000+10*ranks[i][3];
+
+		else if(two_pair(ranks,suits,i) > 0)					//two-pair
+		{
+			int tmp=two_pair(ranks,suits,i);
+
+			if(tmp == 1) 
+				ans[i] = 3000+10*ranks[i][4]+suits[i][4];
+
+			if(tmp==2 || tmp==3)
+                        	ans[i] = 3000+10*ranks[i][5]+suits[i][5];
+		}
+
+		else if(pair(ranks,suits,i) > 0) 					//pair
+		{
+			int tmp=pair(ranks,suits,i);
+
+			for(int k=1;k<=4;k++)
+			{
+				if(tmp == k) 
+					ans[i] = 2000+10*ranks[i][k+1]+suits[i][k+1];
+			}
+		}
+
+		else 
+			ans[i] = 1000+10*ranks[i][5]+suits[i][5];
+		
+	}
+
+	char print[5]={' ','A','B','C','D'};
+	for(int i=1;i<people;i++)
+	{
+		for(int j=i+1;j<=people;j++)
+		{
+			if(ans[j] > ans[i])
+			{
+				ans[j] ^= ans[i] ^= ans[j] ^= ans[i];
+				print[i] ^= print[j] ^= print[i] ^= print[j];
+			}
 		}
 	}
 
 	for(int i=1;i<=people;i++)
-	{
-		if(straight(ranks,i)==1 && flush(suits,i)==1) 			//straight flush
-		{
-			ans[i][1] = 9;
-			ans[i][2] = ranks[i][1];
-			ans[i][3] = suits[i][1];
-		}
+		printf("%c ", print[i]);
 
-		else if(four_of_a_kind(ranks,i) == 1)				//four-of-a-kind
-		{
-			ans[i][1] = 8;
-			ans[i][2] = ranks[i][3];
-		}
-
-		else if(full_house(ranks,i) == 1)				//full-house
-		{
-			ans[i][1] = 7;
-			ans[i][2] = ranks[i][3];
-		}
-		
-		else if(flush(suits,i) == 1) 					//flush
-		{
-			ans[i][1] = 6;
-			ans[i][2] = suits[i][1];
-		}
-
-		else if(straight(ranks,i) == 1)					//straight
-		{
-			ans[i][1] = 5;
-			ans[i][2] = ranks[i][1];
-		}
-		
-		else if(three_of_a_kind(ranks,suits,i) == 1)			//three-of-a-kind
-		{
-			ans[i][1] = 4;
-			ans[i][2] = ranks[i][3];
-		}
-
-		else if(two_pair(ranks,i) > 0)					//two-pair
-		{
-			int tmp=two_pair(ranks,i);
-			ans[i][1] = 3;
-			if(tmp == 1) 
-			{
-				if(ranks[i][1]>ranks[i][3]) 
-				{
-					ans[i][2] = ranks[i][1];
-					ans[i][3] = (suits[i][1]>suits[i][2]) ? suits[i][1] : suits[i][2];
-				}
-				else
-				{
-					ans[i][2] = ranks[i][3];
-					ans[i][3] = (suits[i][3]>suits[i][4]) ? suits[i][3] : suits[i][4];
-				}
-			}
-                        if(tmp == 2)
-                        {
-                                if(ranks[i][2]>ranks[i][4])
-                                {
-                                        ans[i][2] = ranks[i][2];
-                                        ans[i][3] = (suits[i][2]>suits[i][3]) ? suits[i][2] : suits[i][3];
-                                }
-                                else
-                                {
-                                        ans[i][2] = ranks[i][4];
-                                        ans[i][3] = (suits[i][4]>suits[i][5]) ? suits[i][4] : suits[i][5];
-                                }
-                        }
-			if(tmp == 3)
-                        {
-                                if(ranks[i][1]>ranks[i][4])
-                                {
-                                        ans[i][2] = ranks[i][1];
-                                        ans[i][3] = (suits[i][1]>suits[i][2]) ? suits[i][1] : suits[i][2];
-                                }
-                                else
-                                {
-                                        ans[i][2] = ranks[i][4];
-                                        ans[i][3] = (suits[i][4]>suits[i][5]) ? suits[i][4] : suits[i][5];
-                                }
-                        }
-		}
-
-		else if(pair(ranks,i) > 0) 						//pair
-		{
-			int tmp=pair(ranks,i);
-			ans[i][1] = 2;
-			if(tmp == 1)
-                                ans[i][2] = (suits[i][1]>suits[i][2]) ? suits[i][1] : suits[i][2];
-                        if(tmp == 2)
-                                ans[i][2] = (suits[i][2]>suits[i][3]) ? suits[i][2] : suits[i][3];
-                        if(tmp == 3)
-                                ans[i][2] = (suits[i][3]>suits[i][4]) ? suits[i][3] : suits[i][4];
-                        if(tmp == 4)
-                                ans[i][2] = (suits[i][4]>suits[i][5]) ? suits[i][4] : suits[i][5];			
-		}
-
-		else 
-		{
-			sort(ranks,suits,i);
-
-			ans[i][1] = 1;
-			ans[i][2] = ranks[i][5];
-		}
-	}
-
-
-
-/*	 for(int i=1;i<=people;i++)
-	 {
-                for(int j=1;j<=cards;j++) printf("%d %d\t", ranks[i][j], suits[i][j]);
-		puts("");
-	 }
-*/
+	return 0;
 }
