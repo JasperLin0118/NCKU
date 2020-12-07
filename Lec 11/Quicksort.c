49f3a436c5f72aa0100deafa172bfd2057693f8d
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-12-07 20:12:13 +0800

    Lec 11

--- Lec 11/Quicksort.c
+++ Lec 11/Quicksort.c
@@ -11,7 +11,7 @@ void quicksort(int a[], int low, int high)
 		}
         	a[pivot] ^= a[j] ^= a[pivot] ^= a[j];
 	
-       		 quicksort(a,j+1,high);
+       		quicksort(a,j+1,high);
 		quicksort(a,low,j-1);
 	}
 }
