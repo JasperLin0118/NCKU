7ad864b98bcb5c5b8084ede6afeb8aedbcf0aaa1
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2020-12-07 20:28:25 +0800

    Lec 11

--- Lec 11/Quicksort.c
+++ Lec 11/Quicksort.c
@@ -7,9 +7,9 @@ void quicksort(int a[], int low, int high)
 		{
 			while(a[i] <= a[pivot] && i <= high) i++;
 			while(a[j] >= a[pivot] && j > low) j--;
-			if(j >= i) a[i] ^= a[j] ^= a[i] ^= a[j];
+			if(j >= i) int tmp=a[i], a[i]=a[j], a[j]=tmp;	//swap
 		}
-        	a[pivot] ^= a[j] ^= a[pivot] ^= a[j];
+        	int tmp=a[pivot], a[pivot]=a[j], a[j]=tmp;	//swap
 	
        		quicksort(a,j+1,high);
 		quicksort(a,low,j-1);
