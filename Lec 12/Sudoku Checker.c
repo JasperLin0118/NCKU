c91750ecbd125db8867a0f3916d19f33d87b72d1
Dannyyang0329 <f7409467@gs.ncku.edu.tw>
2020-12-10 22:52:13 +0800

    Lec 12

--- Lec 12/Sudoku Checker.c
+++ Lec 12/Sudoku Checker.c
@@ -1,16 +1,30 @@
-void check_sudodu(int (*grid_p)[NUM])					//equal to int grid_p[NUM][NUM}
-{
-	for(int i=0;i<NUM;i++)						//scan all element
+void check_sudoku(int (*grid_p)[NUM])									//equal to int grid_p[NUM][NUM}
+{	
+	for(int i=0;i<NUM;i++)										//scan all element
 	{
 		for(int j=0;j<NUM;j++)
 		{
-			for(int k=0;k<NUM;k++)				//row test
+			int pt_once=0, element=grid_p[i][j];
+			for(int k=0;k<NUM && pt_once==0;k++)						//row test
 			{
-				if(k == j) continue;
-				if(grid[i][j]==gird[k][j]) printf("(%d,%d)\n", i, j);
+				if(k == i) continue;
+				if(element==grid_p[k][j]) printf("(%d,%d)\n", i, j), pt_once=1;
 			}
 
-			for(int k=1;k<NUM;k++)
+			for(int k=0;k<NUM && pt_once==0;k++)						//column test
+			{
+				if(k == j) continue;
+				if(element==grid_p[i][k]) printf("(%d,%d)\n", i, j), pt_once=1;
+			}
+
+			for(int m=3*(i/3);m<3*(i/3)+3 && pt_once==0;m++)				//square test
+			{
+				for(int n=3*(j/3);n<3*(j/3)+3 && pt_once==0;n++)
+				{
+					if(m==i && n==j) continue;
+					if(element==grid_p[m][n]) printf("(%d,%d)\n", i, j), pt_once=1;
+				}
+			}
 		}
 	}
 }
